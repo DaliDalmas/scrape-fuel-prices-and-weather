f90feb0b2e1af6737895e1dfc855488d5ad53c4b
Dalmas.Otieno <dalmas.otieno@GLB-1602019-1779.local>
2022-10-13 22:15:46 +0300

    added dags

---
+++ airflow/dags/run_hourly_tasks.py
@@ -0,0 +1,77 @@
+from airflow import DAG
+from datetime import datetime
+from airflow.operators.python import PythonOperator
+
+from fetch.fuel import Fuel
+from fetch.weather import Weather
+from scrape.scrape_fuel import ScrapeFuel
+from scrape.scrape_weather import ScrapeWeather
+
+
+dag = DAG(
+    dag_id="run_hourly_tasks",
+    start_date=datetime(2016, 1, 1),
+    schedule="@hourly"
+    )
+
+run_fetch_kenyan_fuel = PythonOperator(
+    dag=dag,
+    task_id = 'run_fetch_kenyan_fuel',
+    python_callable=Fuel('Kenya').fetch_fuel()
+)
+
+run_fetch_ugandan_fuel = PythonOperator(
+    dag=dag,
+    task_id = 'run_fetch_ugandan_fuel',
+    python_callable=Fuel('Uganda').fetch_fuel()
+)
+
+run_fetch_kenyan_weather = PythonOperator(
+    dag=dag,
+    task_id = 'run_fetch_kenyan_weather',
+    python_callable=Weather(251, 'nairobi').fetch_weather()
+)
+
+run_fetch_ugandan_weather = PythonOperator(
+    dag=dag,
+    task_id = 'run_fetch_ugandan_weather',
+    python_callable=Weather(1328, 'kampala').fetch_weather()
+)
+
+run_scrape_kenyan_fuel = PythonOperator(
+    dag=dag,
+    task_id = 'run_scrape_kenyan_fuel',
+    python_callable=ScrapeFuel('tmp/kenya_fuel_price.html').scrape('KE')
+)
+
+run_scrape_ugandan_fuel = PythonOperator(
+    dag=dag,
+    task_id = 'run_scrape_ugandan_fuel',
+    python_callable=ScrapeFuel('tmp/uganda_fuel_price.html').scrape('UG')
+)
+
+run_scrape_kenyan_weather = PythonOperator(
+    dag=dag,
+    task_id = 'run_scrape_kenyan_weather',
+    python_callable=ScrapeWeather('tmp/kampala_weather.html').scrape('UG')
+)
+
+run_scrape_ugandan_weather = PythonOperator(
+    dag=dag,
+    task_id = 'run_scrape_ugandan_weather',
+    python_callable=ScrapeWeather('tmp/nairobi_weather.html').scrape('KE')
+)
+
+def clean_up_files():
+    pass
+
+run_clean_up = PythonOperator(
+    dag=dag,
+    task_id = 'run_clean_up',
+    python_callable=clean_up_files
+)
+
+run_fetch_kenyan_fuel >> run_scrape_kenyan_fuel >> run_clean_up
+run_fetch_ugandan_fuel >> run_scrape_ugandan_fuel >> run_clean_up
+run_fetch_kenyan_weather >> run_scrape_kenyan_weather >> run_clean_up
+run_fetch_ugandan_weather >> run_scrape_ugandan_weather >> run_clean_up
